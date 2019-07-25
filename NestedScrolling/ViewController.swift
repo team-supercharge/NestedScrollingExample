f4d904bc0c4d8a0234f361136550a9444e1e6409
Csabi Vido <csaba.vido@supercharge.io>
2019-07-25 13:07:16 +0200

    Initial Commit

---
+++ NestedScrolling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NestedScrolling
+//
+//  Created by Csaba Vidó on 2019. 07. 25..
+//  Copyright © 2019. Csaba Vidó. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
